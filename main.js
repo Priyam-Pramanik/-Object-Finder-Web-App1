b7905653c81ebbd381cc3efc3d5a883006b37fc0
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2024-06-16 21:59:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
+stat= "";
 function setup(){
     canvas = createCanvas(640, 440);
-    canvas.center
+    canvas.center();
+
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById('status').innerHTML = "Model Status: Detecting Objects...";
+    text_box = document.getElementById('text_box').value;
+}
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+    stat = true;
+}
+
+function draw(){
+    image(video, 0, 0, 640, 440);
 }
